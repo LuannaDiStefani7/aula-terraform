92914a070636d33a34d38b73d9126ea8526aa924
LuannaDiStefani7 <luanna.souza@sptech.school>
2024-08-22 08:38:46 -0300

    feat(): Terraform em arquivso diferentes

--- ec2-1.tf
+++ ec2-1.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "ec2-terraform1" { // nome usado pra controle do terraform
   ami = "ami-0e86e20dae9224db8" // codigo que descreve o tipo de instancia ec2
   instance_type = "t2.small" // tamanho da instancia
+    ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 40
+    volume_type = "gp3"
+  }
   tags = {
     Name = "ec2-terraform1-separado" // nome que ira aparecer na aws 
   }
